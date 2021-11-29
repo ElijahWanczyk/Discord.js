21751ae4e6bbd16344b858f69855f0576f904c88
jingpower <81255928+ElijahWanczyk16@users.noreply.github.com>
2021-11-29 21:58:07 +0000

    Added

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log("I'm in");
+  console.log(client.user.username);
+});
+
+client.on('message', msg => {
+    if (msg.author.id != client.user.id) {
+        msg.channel.send(msg.content.split('').reverse().join(''));
+    }
+});
+
+client.login(process.env.TOKEN)
