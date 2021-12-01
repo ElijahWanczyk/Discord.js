26dc7003a4e943d5a19f9574645ed5866a971cad
ElijahWanczyk16 <81255928+ElijahWanczyk16@users.noreply.github.com>
2021-12-01 06:28:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,32 @@ bot.on('message', async message => {
     switch (command) {
 
       case 'ping':
+const { Client, MessageEmbed } = require('discord.js');
+const config = require('./config');
+const commands = require('./help');
+
+let bot = new Client({
+  fetchAllMembers: true, // Remove this if the bot is in large guilds.
+  presence: {
+    status: 'idle',
+    activity: {
+      name: `${config.prefix}help | s-av.invite soon!`,
+      type: 'PLAYING'
+    }
+  }
+});
+
+bot.on('ready', () => console.log(`Logged in as ${bot.user.tag}.`));
+
+bot.on('message', async message => {
+  // Check for command
+  if (message.content.startsWith(config.prefix)) {
+    let args = message.content.slice(config.prefix.length).split(' ');
+    let command = args.shift().toLowerCase();
+
+    switch (command) {
+
+      case 'ping':
         let msg = await message.reply('Pinging...');
         await msg.edit(`PONG! Message round-trip took ${Date.now() - msg.createdTimestamp}ms.`)
         break;
